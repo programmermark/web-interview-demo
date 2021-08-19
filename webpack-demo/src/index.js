37b1b7be675ba21fcc0281396b7d22d2b0fe5b3e
钟辉 <hui.zhong@wholion.com>
2021-08-19 18:15:13 +0800

    feat: 新增笔记

---
+++ webpack-demo/src/index.js
@@ -0,0 +1,5 @@
+const sum = (a, b) => a + b;
+
+const result = sum(10, 30);
+
+console.log("result", result);
---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        include: path.join(__dirname, "src"),
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    port: 5000,
+    contentBase: path.join(__dirname, "dist"),
+  },
+};
